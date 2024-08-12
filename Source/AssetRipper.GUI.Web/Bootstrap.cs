602e44877079e9f16c58e66a79580163beddde25
Jesse Zhu <jesse@itjesse.com>
2024-08-12 17:01:57 +0900

    Update Bootstrap.cs

--- Source/AssetRipper.GUI.Web/Bootstrap.cs
+++ Source/AssetRipper.GUI.Web/Bootstrap.cs
@@ -7,7 +7,7 @@ internal static class Bootstrap
 		new Link(writer)
 		{
 			Rel = "stylesheet",
-			Href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+			Href = "https://jsdelivr.srkyxk.com/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
 			Integrity = "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN",
 			Crossorigin = "anonymous"
 		}.Close();
@@ -17,7 +17,7 @@ internal static class Bootstrap
 	{
 		new Script(writer)
 		{
-			Src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+			Src = "https://jsdelivr.srkyxk.com/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
 			Integrity = "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL",
 			Crossorigin = "anonymous"
 		}.Close();
